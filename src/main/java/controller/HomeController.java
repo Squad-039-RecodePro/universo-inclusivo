5961f743cd81a4d3ecfbe0043645480a4350d2bd
Thiago Queiroz <thiagoq.dev@gmail.com>
2023-11-20 20:31:05 -0300

    atualização style

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -5,12 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@GetMapping
-	public String home () {
-	
+	public String home() {
+
 		return "index";
 	}
+
+	@GetMapping("/contato")
+	public String contato() {
+
+		return "contato";
+	}
+
+	@GetMapping("/sobre")
+	public String sobre() {
+
+		return "sobre";
+	}
 }
-
-
